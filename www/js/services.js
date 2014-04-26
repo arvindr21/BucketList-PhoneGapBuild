6e36b6aecb08609778d26997f9b6c1e141926532
Arvind Ravulavaru <arvind.ravulavaru@gmail.com>
2014-04-27 03:21:40 +0530

    Update

--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,6 @@
 angular.module('bucketList.services', [])
     .factory('API', function ($rootScope, $http, $ionicLoading, $window) {
+       var base = "http://bucketlistapplication.herokuapp.com";
         $rootScope.show = function (text) {
             $rootScope.loading = $ionicLoading.show({
                 content: text ? text : 'Loading',
@@ -35,13 +36,13 @@ angular.module('bucketList.services', [])
 
         return {
             signin: function (form) {
-                return $http.post('http://bucketlist.cloudno.de/api/v1/bucketList/auth/login', form);
+                return $http.post(base+'/api/v1/bucketList/auth/login', form);
             },
             signup: function (form) {
-                return $http.post('http://bucketlist.cloudno.de/api/v1/bucketList/auth/register', form);
+                return $http.post(base+'/api/v1/bucketList/auth/register', form);
             },
             getAll: function (email) {
-                return $http.get('http://bucketlist.cloudno.de/api/v1/bucketList/data/list', {
+                return $http.get(base+'/api/v1/bucketList/data/list', {
                     method: 'GET',
                     params: {
                         token: email
@@ -49,7 +50,7 @@ angular.module('bucketList.services', [])
                 });
             },
             getOne: function (id, email) {
-                return $http.get('http://bucketlist.cloudno.de/api/v1/bucketList/data/list/' + id, {
+                return $http.get(base+'/api/v1/bucketList/data/list/' + id, {
                     method: 'GET',
                     params: {
                         token: email
@@ -57,7 +58,7 @@ angular.module('bucketList.services', [])
                 });
             },
             saveItem: function (form, email) {
-                return $http.post('http://bucketlist.cloudno.de/api/v1/bucketList/data/item', form, {
+                return $http.post(base+'/api/v1/bucketList/data/item', form, {
                     method: 'POST',
                     params: {
                         token: email
@@ -65,7 +66,7 @@ angular.module('bucketList.services', [])
                 });
             },
             putItem: function (id, form, email) {
-                return $http.put('http://bucketlist.cloudno.de/api/v1/bucketList/data/item/' + id, form, {
+                return $http.put(base+'/api/v1/bucketList/data/item/' + id, form, {
                     method: 'PUT',
                     params: {
                         token: email
@@ -73,7 +74,7 @@ angular.module('bucketList.services', [])
                 });
             },
             deleteItem: function (id, email) {
-                return $http.delete('http://bucketlist.cloudno.de/api/v1/bucketList/data/item/' + id, {
+                return $http.delete(base+'/api/v1/bucketList/data/item/' + id, {
                     method: 'DELETE',
                     params: {
                         token: email
